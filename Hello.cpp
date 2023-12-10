79a596d7c795f5d18ca4d809c3fe63bcf2e5465b
unknown <gustavofring894@gmail.com>
2023-12-10 11:14:40 +0500

    I added a for loop which will print Hello world 5 times !

--- Hello.cpp
+++ Hello.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main()		{
     // Record the start time
     auto start = chrono::high_resolution_clock::now();
-
-    cout << "Hello World !" << endl;
-    cout << "Hello World !" << endl;
+    
+    for(int i=1; i<=5; i++)
+        cout << "Hello World !" << endl;
 
     // Record the end time
     auto end = chrono::high_resolution_clock::now();
